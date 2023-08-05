2efa39c1e5e75ff1420d04dae61c197142910e0e
sdd2cxz <sdd2cxz@gmail.com>
2023-08-05 13:42:08 +0200

    minor changes

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const btn = document.getElementById("btn");
 const inputfield = document.getElementById('input')
 const showColors = document.getElementById('show-colors')
-let textColor = document.getElementById("text")
 
-let colorArray = ["black","red", "blue", "yellow"]
+let colorArray = []
 
 colorArray = JSON.parse(localStorage.getItem('color'))
 
@@ -18,11 +16,10 @@ function changeColor(){
         text.style.color = "black"
     }
     console.log(document.body.style.backgroundColor)
-    //console.log(text.style.color)
 }
 const color = document.getElementById('btn')
 const input = document.getElementById('disable')
-color.addEventListener('click', function(e) {
+color.addEventListener('click', function() {
     if(input.style.display === "none"){
         input.style.display = "block"
     }else{
@@ -33,12 +30,13 @@ color.addEventListener('click', function(e) {
     console.log(input.style.display)
     //e.target.style.visibility = "hidden"
 })
+// checks if value is a valid color
 function isColor(strColor){
     var s = new Option().style;
     s.color = strColor;
     return s.color == strColor.toLowerCase();
   }
-
+// adds color to the list or returns error when color is invalid
 function addColor(){
  if(!isColor(inputfield.value)){
     console.log('e')
@@ -47,14 +45,13 @@ function addColor(){
      colorArray.push(inputfield.value)
      localStorage.setItem('color', JSON.stringify(colorArray))
      location.reload()
-     console.log(colorArray)
     }
     
         
     
 }
 
-
+// removes every color from list
 function removeColors(){
     colorArray = []
     localStorage.setItem('color', JSON.stringify(colorArray))
